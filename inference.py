fad5a3a8e5b5d2b90a732bee6c4395fafe85b68d
SamN55 <zosigiv@gmail.com>
2022-05-18 13:30:38 +0300

    class predictor files added

---
+++ inference.py
@@ -0,0 +1,46 @@
+"""
+Load a model to predict user class
+"""
+import numpy as np
+import pickle
+from flask import Flask
+from flask import request
+import pandas as pd
+import warnings
+
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+
+
+@app.route('/predict_class')
+def predict_class():
+    clf = read_model('class_model.pkl')
+    age = request.args.get('age')
+    is_male = request.args.get('is_male')
+    height_cm = request.args.get('height_cm')
+    weight_kg = request.args.get('weight_kg')
+    body_fat = request.args.get('body_fat')  ## name reformating
+    sit_ups_counts = request.args.get('sit_ups_counts')
+    broad_jump_cm = request.args.get('broad_jump_cm')
+
+    d = {'is_male': [is_male], 'height_cm': [height_cm], 'weight_kg': [weight_kg], 'age': [age],
+         'body_fat': [body_fat], 'sit_ups_counts': [sit_ups_counts], 'broad_jump_cm': [broad_jump_cm]}
+    X = pd.DataFrame(d)
+    y = clf.predict(X)
+
+    return str(y[0])
+
+
+def read_model(filename):
+    with open(filename, 'rb') as f:
+        lr = pickle.load(f)
+    return lr
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
